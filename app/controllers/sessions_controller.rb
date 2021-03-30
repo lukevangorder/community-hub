585f0a3cd6aac427a088e54705673c7238b1cf02
lukevangorder <lukevangorder@gmail.com>
2021-03-29 22:05:59 -0400

    Added sessions#create and sessions#register to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,14 +4,25 @@ class SessionsController < ApplicationController
     end
 
     def create
-        session[:username] = params[:username]
-        redirect_to '/home'
+        @user = User.find_by(username: params[:login][:identification])
+        if @user == nil
+            @user = User.find_by(email: params[:login][:identification])
+        end
+        if @user.authenticate(params[:password])
+            sessions[:user_id] = @user.id
+            redirect_to '/home'
+        end # Add error handling here
     end
 
     def signup #user sign up
     end
 
     def register
+        @user = User.new(username: params[:login][:username], email: params[:login][:email], password: params[:login][:password], name: params[:login][:name])
+        if @user.save
+            sessions[:user_id] = @user.id
+            redirect_to '/home'
+        end # Add error handling here
     end
 
 end
