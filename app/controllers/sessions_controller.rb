e33b309a82e5d4cf1b8f8360b01f312d2864dd21
lukevangorder <lukevangorder@gmail.com>
2021-03-30 15:49:00 -0400

    Added dotenv and omniauth gems for off site login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,10 +11,12 @@ class SessionsController < ApplicationController
         if @user == nil
             @user = User.find_by(email: params[:login][:identification])
         end
-        if @user.authenticate(params[:password])
-            sessions[:user_id] = @user.id
+        if @user.authenticate(params[:login][:password])
+            session[:user_id] = @user.id
             redirect_to '/home'
-        end # Add error handling here
+        else # Add error handling here
+            redirect_to '/'
+        end
     end
 
     def signup #user sign up
@@ -23,9 +25,14 @@ class SessionsController < ApplicationController
     def register
         @user = User.new(username: params[:login][:username], email: params[:login][:email], password: params[:login][:password], name: params[:login][:name])
         if @user.save
-            sessions[:user_id] = @user.id
+            session[:user_id] = @user.id
             redirect_to '/home'
         end # Add error handling here
+    end
+
+    def logout
+        session.delete :user_id
+        redirect_to '/'
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
   get "/", to: "sessions#foyer"
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   get "/signup", to: "sessions#signup"
   post "/signup", to: "sessions#register"
+  get "/home", to: "users#home"
+  get "/logout", to: "sessions#logout"
 end
