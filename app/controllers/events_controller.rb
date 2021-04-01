c63c827c0b95b2f25f8c2b3f0faa71392de3d798
lukevangorder <lukevangorder@gmail.com>
2021-04-01 14:16:13 -0400

    Added events#create to handle creation of valid events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -11,7 +11,11 @@ class EventsController < ApplicationController
     end
 
     def create
-        binding.pry
+        event = Event.new(name: params[:event][:name], info: params[:event][:info], starts_at: params[:event][:starts_at], ends_at: params[:event][:ends_at])
+        if event.save
+            User.find(sessions[:user_id]).events < event
+            redirect_to '/events'
+        end #add error handling
     end
 
 end
