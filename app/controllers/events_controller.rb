2f40f37775a68398eab771f24c49af837ba24786
lukevangorder <lukevangorder@gmail.com>
2021-04-04 08:07:41 -0400

    Added collection check boxes to events field to create location with event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,9 +8,11 @@ class EventsController < ApplicationController
 
     def new
         @event = Event.new
+        @locations = Location.all
     end
 
     def create
+        binding.pry
         event = Event.new(event_params)
         event.user = User.find(session[:user_id])
         if event.save
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ActiveRecord::Base
     belongs_to :user #belongs to a user that created it
     belongs_to :location
+    accepts_nested_attributes_for :location
 end
